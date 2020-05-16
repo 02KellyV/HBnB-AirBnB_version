33d2bbbf87f6e350b28785e6869e3c6a04343f79
andresmelek <candres.isaza@gmail.com>
2020-05-16 08:15:15 -0500

    advance on task 0 and 1

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,23 @@
+const amenities = {};
+$(document).ready(function () {
+	console.log('entre')
+  $('input[type="checkbox"]').click(function () {
+	  console.log('hola')
+    if ($(this).prop('checked') === true) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    $('.amenities H4').text('');
+    let count = 0;
+    Object.entries(amenities).forEach(([key, value]) => {
+      if (count === 0) {
+        $('.amenities H4').append(value);
+      } else {
+        $('.amenities H4').append(', ');
+        $('.amenities H4').append(value);
+      }
+      count += 1;
+    });
+  });
+});
